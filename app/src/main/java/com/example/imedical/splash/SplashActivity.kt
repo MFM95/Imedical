0b8e4d7fd2be01c43d23c4ef3a3ec427d954c8b5
ahmed.hassan.dev@gmail.com <ahmed.hassan.dev@gmail.com>
2019-08-17 11:50:23 +0200

    splash activity with timer

---
+++ app/src/main/java/com/example/imedical/splash/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.example.imedical.splash
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.imedical.R
+import com.example.imedical.login.presentation.view.activity.LoginActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(1000)
+            CoroutineScope(Dispatchers.Main).launch {
+                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            }
+        }
+    }
+}
